a300d3b3d24be6b78963d2ea6d65611e615998b8
Martynas <packmaster@riawolf.com>
2018-01-31 10:24:40 +0200

    Cleanup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /*
+        * Parašyti programą,
+        * kuri leistų vartotojui įvesti norimą skaičių
+        * ir po to parašytų ar šis skaičius yra lyginis ar ne lyginis.
+        * Užuomina panaudokite % operatoriu.
+        * */
 
         System.out.println("Iveskite sveika skaiciu");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a%2 == 0){
+        if (a % 2 == 0) {
             System.out.println("Skaicius yra lyginis");
         } else {
             System.out.println("Skaicius yra nelyginis");
